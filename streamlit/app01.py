a5c62938df2ab4474f7794f73e49c95d928d07e8
thanin66 <thanin.ra.66@ubu.ac.th>
2024-02-14 15:18:21 +0700

    message

---
+++ streamlit/app01.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.header("My OOP Notebook")
+
---
+++ streamlit/app02.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+
+st.header("My OOP Notebook")
+st.title("OOP")
+st.sidebar('go')
+st.sidebar('went')
+st.sidebar('gone')
