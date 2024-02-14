e2bd036de6f346d3a79ef5d2e3ea3c4c1496bc85
thanin66 <thanin.ra.66@ubu.ac.th>
2024-02-14 20:43:20 +0700

    add code

--- streamlit/app02.py
+++ streamlit/app02.py
@@ -1,8 +1,8 @@
 import streamlit as st
-
-
-st.header("My OOP Notebook")
-st.title("OOP")
-st.sidebar('go')
-st.sidebar('went')
-st.sidebar('gone')
+st.sidebar.text('Sidebar')
+st.sidebar.button('sa')
+st.sidebar.button('wat>')
+st.sidebar.button('dee')
+st.title('Hello')
+st.image('https://media0.giphy.com/media/xUA7b5ix0igTG9CyU8/giphy.gif')
+st.text_input("What's your name?")
